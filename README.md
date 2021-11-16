d02d41709e8bfe446afc083f8189ea1317b56c61
alexsdopstudio <alexsallemi@icloud.com>
2021-11-16 17:35:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wqjsej
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wqjsej)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+document.getElementById('app').innerHTML += ' World';
